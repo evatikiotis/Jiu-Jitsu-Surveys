b8006a935759b70df1dacc76ad03666e851206c6
Evangelos Vatikiotis <evangelos.vatikiotis@capgemini.com>
2021-02-07 16:35:05 +0100

    fix backend utl in side docker compose

--- src/pages/surveys/index.tsx
+++ src/pages/surveys/index.tsx
@@ -2,28 +2,28 @@ import SurveyListItem from "../../components/SurveyListItem";
 
 export default function SurveysList(props: any) {
   // eslint-disable-next-line react/prop-types
-  const { surveys } = props;
+  const {surveys} = props;
   return (
     <>
       <div>
         <div>
           <table className="table">
             <thead>
-              <tr>
-                <th className="display-none-small-screen" scope="col">
-                  {" "}
-                  #{" "}
-                </th>
-                <th scope="col"> Survey Name </th>
-                <th scope="col"> Take the Survey </th>
-                <th scope="col"> Survey Analytics </th>
-                <th scope="col"> Created Date </th>
-              </tr>
+            <tr>
+              <th className="display-none-small-screen" scope="col">
+                {" "}
+                #{" "}
+              </th>
+              <th scope="col"> Survey Name</th>
+              <th scope="col"> Take the Survey</th>
+              <th scope="col"> Survey Analytics</th>
+              <th scope="col"> Created Date</th>
+            </tr>
             </thead>
             <tbody>
-              {surveys.map((survey: any, index: any) => (
-                <SurveyListItem key={survey.id} survey={survey} index={index} />
-              ))}
+            {surveys.map((survey: any, index: any) => (
+              <SurveyListItem key={survey.id} survey={survey} index={index}/>
+            ))}
             </tbody>
           </table>
         </div>
@@ -37,7 +37,8 @@ export default function SurveysList(props: any) {
 export async function getStaticProps() {
   // Call an external API endpoint to get posts.
   // You can use any data fetching library
-  const res = await fetch("http://localhost:8080/surveys");
+  // TODO: url to be passed as environment variable
+  const res = await fetch("http://backend:8080/surveys");
   const surveys = await res.json();
   // By returning { props: posts }, the Blog component
   // will receive `posts` as a prop at build time
