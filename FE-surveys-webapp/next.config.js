8c0eaf154b8caf1abccf6aeb67d840b51f192659
Evangelos Vatikiotis <evangelos.vatikiotis@capgemini.com>
2024-05-30 12:42:22 +0200

    prismic nextjs config

---
+++ FE-surveys-webapp/next.config.js
@@ -0,0 +1,13 @@
+g/** @type {import('next').NextConfig} */
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.prismic.io",
+        port: "",
+        pathname: "/my-account/**",
+      },
+    ],
+  },
+};
