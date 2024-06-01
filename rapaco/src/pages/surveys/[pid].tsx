63208886a7efa81c886e16a2e33aa5123d121517
Evangelos Vatikiotis <evangelos.vatikiotis@capgemini.com>
2024-06-01 14:28:10 +0200

    cleanup

---
+++ rapaco/src/pages/surveys/[pid].tsx
@@ -0,0 +1,20 @@
+import {useRouter} from 'next/router';
+
+export default function SurveyForm() {
+  const router = useRouter();
+  const {pid} = router.query;
+  return (
+    <div>
+      <iframe
+        src={`https://docs.google.com/forms/d/${pid}/viewform?embedded=true`}
+        width={640}
+        height={692}
+        frameBorder={0}
+        marginHeight={0}
+        marginWidth={0}
+      >
+        Loading…
+      </iframe>
+    </div>
+  );
+}
